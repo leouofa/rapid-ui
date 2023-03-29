e4ad0303d4fc6be94cd2462f646fdbd6f686268b
Leonid Medovyy <leonid@storypro.io>
2023-03-29 16:54:06 -0700

    refactor monkeypatch

--- lib/install/render_monkeypatch.rb
+++ lib/install/render_monkeypatch.rb
@@ -2,14 +2,12 @@
 
 # adding r as an alias to the render method
 # to make rendering our ui components easier on the eyes
-if defined?(ActionView::Helpers::RenderingHelper)
-  module ActionView
-    module Helpers
-      module RenderingHelper
-        alias r render
-      end
+raise "Cant monkeypatch ActionView::Helpers::RenderingHelper" unless defined?(ActionView::Helpers::RenderingHelper)
+
+module ActionView
+  module Helpers
+    module RenderingHelper
+      alias r render
     end
   end
-else
-  raise 'Cant monkeypatch ActionView::Helpers::RenderingHelper'
 end
